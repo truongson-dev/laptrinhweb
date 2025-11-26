4879707309dbe0d71654b993f53d7a7f35b4c651
TRƯƠNG THANH SƠN <son.truong27@student.passerellesnumeriques.org>
2025-11-26 08:32:35 +0700

    update

---
+++ login.php
@@ -0,0 +1,9 @@
+ <h2>Form Đăng nhập</h2>    
+    <form method="POST" action="xuly.php">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username"><br><br>
+        
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        
+        <input type="submit" value="Đăng nhập">
